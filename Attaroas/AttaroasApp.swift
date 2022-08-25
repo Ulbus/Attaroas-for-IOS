306c7132df094e353da34f697bd3334810f3ae68
Ulbus <91960142+Ulbus@users.noreply.github.com>
2022-08-25 14:17:14 +0700

    Initial Commit

---
+++ Attaroas/AttaroasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttaroasApp.swift
+//  Attaroas
+//
+//  Created by Rawiphon Kumpapan on 25/8/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct AttaroasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
