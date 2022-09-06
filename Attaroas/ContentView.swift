d21c9e797e93ea0f4a78cc28ca6975289d14e05f
Ulbus <91960142+Ulbus@users.noreply.github.com>
2022-09-06 10:26:49 +0700

    Update ContentView.swift
    add biding state

--- Attaroas/ContentView.swift
+++ Attaroas/ContentView.swift
@@ -4,16 +4,49 @@
 //
 //  Created by Rawiphon Kumpapan on 25/8/2565 BE.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email: String = ""
+    @State private var password: String = "";
+    @State  var logined: Bool = false
     var body: some View {
-        Text("Hello, world!")
+        if self.logined == false{
+        HStack{
+    VStack{
+Text("Attaroas")
+        TextField("Enter your email: ", text: $email)
             .padding()
+        HStack{
+            VStack{
+            }
+        }
+        HStack{
+            VStack{
+        }
+        }
+                SecureField("Enter your password: ", text: $password)
+                .padding()
+        Button("Login", action:{
+            self.logined.toggle()
+                    })
+    }
+        }
+        }
+            menuView(logined: $logined)
+}
+}
+struct menuView: View{
+    @Binding var logined: Bool
+    var body: some View{
+        if self.logined{
+            Button("Logout", action: {
+                self.logined.toggle()
+            })
+            Text("Welcome")
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
